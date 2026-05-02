f4702ea1901deace5ba0dbec190d06d05faa227d
Apoorva-Singh3 <97214359+Apoorva-Singh3@users.noreply.github.com>
2026-05-02 22:10:12 +0530

    Added sample image of a achaar

--- script.js
+++ script.js
@@ -35,143 +35,170 @@ const products = [
   {
     name: "Meetha Aam Achaar",
     category: "fruit-based-indian-achaar",
-    price: 299
+    price: 299,
+    image: "images/Meetha_Aam_Achaar.png"
   },
   {
     name: "Kairi Lahsun Achaar",
     category: "fruit-based-indian-achaar",
-    price: 329
+    price: 329,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Aam Gur Achaar",
     category: "fruit-based-indian-achaar",
-    price: 349
+    price: 349,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Chunda (Mango)",
     category: "fruit-based-indian-achaar",
-    price: 319
+    price: 319,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Kaccha Aam Mustard Pickle",
     category: "fruit-based-indian-achaar",
-    price: 339
+    price: 339,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Amla Achaar",
     category: "fruit-based-indian-achaar",
-    price: 289
+    price: 289,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Lemon Achaar",
     category: "fruit-based-indian-achaar",
-    price: 279
+    price: 279,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Sweet Lemon Pickle",
     category: "fruit-based-indian-achaar",
-    price: 299
+    price: 299,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Lime Chilli Pickle",
     category: "fruit-based-indian-achaar",
-    price: 309
+    price: 309,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Jackfruit Pickle (Kathal)",
     category: "fruit-based-indian-achaar",
-    price: 359
+    price: 359,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Karonda Pickle",
     category: "fruit-based-indian-achaar",
-    price: 329
+    price: 329,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Tamarind Pickle",
     category: "fruit-based-indian-achaar",
-    price: 299
+    price: 299,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Sweet Tamarind Achaar",
     category: "fruit-based-indian-achaar",
-    price: 319
+    price: 319,
+    image: "images/meetha-aam-achaar.jpg"
   },
 
   // Indian Chutneys
   {
     name: "Green Chilli Lehsun Chutney",
     category: "indian-chutneys",
-    price: 199
+    price: 199,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Sweet Lemon Chutney",
     category: "indian-chutneys",
-    price: 219
+    price: 219,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Mango Chutney",
     category: "indian-chutneys",
-    price: 229
+    price: 229,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Jimikanda Chutney",
     category: "indian-chutneys",
-    price: 239
+    price: 239,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Tomato Chutney",
     category: "indian-chutneys",
-    price: 199
+    price: 199,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Red Chilli Chutney (Dry Thecha)",
     category: "indian-chutneys",
-    price: 249
+    price: 249,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Sweet Amla Chutney",
     category: "indian-chutneys",
-    price: 229
+    price: 229,
+    image: "images/meetha-aam-achaar.jpg"
   },
 
   // Healthy Bites
   {
     name: "Amla Laddu",
     category: "healthy-bites",
-    price: 249
+    price: 249,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Sukha Amla Bites",
     category: "healthy-bites",
-    price: 219
+    price: 219,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Seed Laddu",
     category: "healthy-bites",
-    price: 299
+    price: 299,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Dry Fruits Laddu",
     category: "healthy-bites",
-    price: 349
+    price: 349,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Spiced Turmeric Powder <br><span class='prepaid-note'>(Buknu)</span>",
     category: "healthy-bites",
-    price: 189
+    price: 189,
+    image: "images/meetha-aam-achaar.jpg"
   },
 
   // Non-Vegetarian Indian Achaar
   {
     name: "Prawn Pickle <br><span class='prepaid-note'>(on prepaid orders)</span>",
     category: "non-vegetarian-achaar",
-    price: 499
+    price: 499,
+    image: "images/meetha-aam-achaar.jpg"
   },
   {
     name: "Chicken Pickle <br><span class='prepaid-note'>(on prepaid orders)</span>",
     category: "non-vegetarian-achaar",
-    price: 449
+    price: 449,
+    image: "images/meetha-aam-achaar.jpg"
   }
 ];
 
@@ -192,8 +219,7 @@ function renderProducts(filter = "all") {
     div.classList.add("product");
 
     div.innerHTML = `
-      <img src="https://via.placeholder.com/300x200">
-      
+      <img src="${p.image}" alt="${p.name}">      
       <div class="product-content">
         <h4>${p.name}</h4>
         <p>₹${p.price}</p>
